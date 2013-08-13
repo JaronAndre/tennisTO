d8745d938208be9cfd3906c2fe5f1614ef231f7d
JaronAndre <jaron.andre@alumni.utoronto.ca>
2013-08-13 12:52:12 -0400

    tes test test ets

--- tennisTO/urls.py
+++ tennisTO/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'tennisTO.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^courts/', include('CourtInfo.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+   # url(r'^admin/', include(admin.site.urls)),
 )
